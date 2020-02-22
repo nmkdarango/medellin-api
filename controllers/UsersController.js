731b0b406a0717df8c4e4830e5f80c53cc723a4e
maui <maui@devf.mx>
2020-02-21 21:47:34 -0500

    Refactor controllers

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -20,26 +20,29 @@ module.exports = {
       .then((user) => res.status(201).json({ message: 'User created!', data: user }))
       .catch((err) => res.status(400).json({ message: 'Error creating user', data: err }));
   },
-  getUsers: (_, res) => {
-    Users.find()
-      .then((users) => res.status(200).json({ message: 'Users obtained!', data: users }))
-      .catch((err) => res.status(404).json({ message: 'Error getting users', data: err }));
+  getUsers: async (_, res) => {
+    const users = await Users.find({ is_active: true });
+    if (!users) res.status(404).json({ message: 'Users not found', data: users });
+    res.status(200).json({ message: 'Users obtained!', data: users });
   },
-  getUserById: (req, res) => {
+  getUserById: async (req, res) => {
     const { id } = req.params;
-    Users.findById(id)
-      .then((user) => res.status(200).json({ message: 'User obtained!', data: user }))
-      .catch((err) => res.status(404).json({ message: 'Error getting user', data: err }));
+    const user = await Users.findOne({ _id: id, is_active: true });
+    if (!user) res.status(404).json({ message: 'User not found', data: user });
+    res.status(200).json({ message: 'User obtained!', data: user });
   },
   updateUserById: (req, res) => {
     const { id } = req.params;
-    Users.findByIdAndUpdate(id, req.body, { new: true, useFindAndModify: false })
+    Users.findOneAndUpdate({ _id: id, is_active: true }, req.body, {
+      new: true,
+      useFindAndModify: false,
+    })
       .then((updatedUser) => res.status(200).json({ message: 'User updated!', data: updatedUser }))
       .catch((err) => res.status(404).json({ message: 'Error updating user', data: err }));
   },
   deleteUserById: (req, res) => {
     const { id } = req.params;
-    Users.findByIdAndUpdate(id, { is_active: false }, { useFindAndModify: false })
+    Users.findOneAndUpdate({ _id: id, is_active: false }, { useFindAndModify: false })
       .then(() => res.status(204).json())
       .catch((err) => res.status(404).json({ message: 'Error deleting user', data: err }));
   },
