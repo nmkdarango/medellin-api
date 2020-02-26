3aa5aa5f2eb07958cec3145aa19f04ca7932728f
maui <maui@devf.mx>
2020-02-25 20:11:35 -0500

    Agregar validación de creación de usuario

--- routes/UsersRoutes.js
+++ routes/UsersRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 
 const router = express.Router();
+const { UsersValidator } = require('../validators');
 const { UsersController } = require('../controllers');
 
-router.post('/users', UsersController.createUser);
+router.post('/users', [UsersValidator.UserCreateValidator], UsersController.createUser);
 router.get('/users', UsersController.getUsers);
 router.get('/users/:id', UsersController.getUserById);
 router.patch('/users/:id', UsersController.updateUserById);
