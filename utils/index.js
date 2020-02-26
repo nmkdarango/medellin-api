6c727803e59385e52a25a70bee195c77a25a21fe
maui <maui@devf.mx>
2020-02-26 15:46:52 -0500

    Borrar middleware sin usar

--- utils/index.js
+++ utils/index.js
@@ -1,23 +1,6 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = {
-  getRequestDate: (req, res, next) => {
-    const date = new Date();
-
-    const seconds = date.getSeconds();
-    const minutes = date.getMinutes();
-    const hour = date.getHours();
-
-    const year = date.getFullYear();
-    const month = date.getMonth(); // beware: January = 0; February = 1, etc.
-    const day = date.getDate();
-
-    const dateString = `${day}/${month}/${year} - ${hour}:${minutes}:${seconds}`;
-
-    // eslint-disable-next-line no-console
-    console.log(`Fecha de petición: ${dateString}`);
-    next();
-  },
   generateToken: (user) => {
     const exp = new Date();
     exp.setDate(exp.getDate() + 1); // Token lasts one day
