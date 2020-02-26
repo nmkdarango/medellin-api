bb0f10a8c57914bfa3fa457e698475d944d57e99
maui <maui@devf.mx>
2020-02-26 16:19:39 -0500

    Proteger rutas de usuario con jwt

--- routes/UsersRoutes.js
+++ routes/UsersRoutes.js
@@ -1,17 +1,22 @@
 const express = require('express');
 
 const router = express.Router();
+const { verifyToken } = require('../middlewares');
 const { UsersValidator } = require('../validators');
 const { UsersController } = require('../controllers');
 
+// Signup and Login
+router.post('/signup', [UsersValidator.UserSignupValidator], UsersController.signupUser);
+router.post('/login', [UsersValidator.UserLoginValidator], UsersController.loginUser);
+
+// Verify token is supplied
+router.use(verifyToken);
+
+// Users CRUD
 router.post('/users', [UsersValidator.UserCreateValidator], UsersController.createUser);
 router.get('/users', UsersController.getUsers);
 router.get('/users/:id', UsersController.getUserById);
 router.patch('/users/:id', UsersController.updateUserById);
 router.delete('/users/:id', UsersController.deleteUserById);
-
-// Signup and Login
-router.post('/signup', [UsersValidator.UserSignupValidator], UsersController.signupUser);
-router.post('/login', [UsersValidator.UserLoginValidator], UsersController.loginUser);
 
 module.exports = router;
