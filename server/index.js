e437d908826c35d20ad422e062fc8fe4182339ee
maui <maui@devf.mx>
2020-02-26 14:18:53 -0500

    Remover middleware de fecha de peticion

--- server/index.js
+++ server/index.js
@@ -7,14 +7,9 @@ const routes = require('../routes');
 
 const PORT = process.env.PORT || 3000;
 
-const { getRequestDate } = require('../utils');
-
 // parse application/x-www-form-urlencoded and application/json
 server.use(bodyParser.urlencoded({ extended: false }));
 server.use(bodyParser.json());
-
-// show the time the request was made
-server.use(getRequestDate);
 
 // import all routes
 server.use('/api/v1', routes);
