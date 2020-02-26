10b4d2abe6997ff53b0a26a88f0d46f48e2b1aa2
danielarango <daniel.arango@nmksoftware.co>
2020-02-25 21:00:38 -0500

    Se arregla el comdo eslint

--- models/Users.js
+++ models/Users.js
@@ -1,56 +1,55 @@
+/* eslint-disable func-names */
+/* eslint-disable consistent-return */
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+
 const SALT_WORK_FACTOR = 10;
 const { Schema } = mongoose;
 
-
 const userSchema = new Schema({
-    firstname: {
-        type: String,
-        trim: true,
-        required: true
-    },
-    lastname: {
-        type: String,
-        trim: true,
-        required: true
-    },
-    email: {
-        type:String,
-        trim: true,
-        required: true
-    },
-    password:String,
-    is_active: {
-        type:Boolean,
-        default: true
-    }
-    
+  firstname: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  lastname: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  email: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  password: String,
+  is_active: {
+    type: Boolean,
+    default: true,
+  },
 });
 
-userSchema.pre('save', function(next) {
-    var user = this;
+userSchema.pre('save', function (next) {
+  const user = this;
 
-    // only hash the password if it has been modified (or is new)
-    if (!user.isModified('password')) return next();
+  // only hash the password if it has been modified (or is new)
+  if (!user.isModified('password')) return next();
 
-    // generate a salt
-    bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
-        if (err) return next(err);
+  // generate a salt
+  bcrypt.genSalt(SALT_WORK_FACTOR, (err, salt) => {
+    if (err) return next(err);
 
-        // hash the password using our new salt
-        bcrypt.hash(user.password, salt, function(err, hash) {
-            if (err) return next(err);
+    // hash the password using our new salt
+    // eslint-disable-next-line no-shadow
+    bcrypt.hash(user.password, salt, (err, hash) => {
+      if (err) return next(err);
 
-            // override the cleartext password with the hashed one
-            user.password = hash;
-            next();
-        });
+      // override the cleartext password with the hashed one
+      user.password = hash;
+      return next();
     });
-
-
+  });
 });
-
 
 const Users = mongoose.model('User', userSchema);
 module.exports = Users;
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,5 @@
 const express = require('express');
 
-const { celebrate, Joi, errors, Segments } = require('celebrate');
-
 const router = express.Router();
 
 router.use(require('./UsersRoutes'));
