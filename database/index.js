7f90ab4fee576c5ba90fb064ed916f8d2ca53107
maui <maui@devf.mx>
2020-02-26 17:40:45 -0500

    Volver homogéneo el código para windows/unix

--- database/index.js
+++ database/index.js
@@ -1,13 +1,11 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(
-  process.env.MONGO_URI,
-  {
+mongoose
+  .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-  },
-)
+  })
   // eslint-disable-next-line no-console
-  .then(() => console.log('✅ Conexión exitosa con Atlas'))
+  .then(() => console.log('-- Database connected --'))
   // eslint-disable-next-line no-console
-  .catch(() => console.log('🚫 Error al conectar con Atlas'));
+  .catch((err) => console.log('Database ERROR: ', err));
